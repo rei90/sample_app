709c9f019a35488ef8b08161a69154b40f2d0d8d
Rei Araki <rei3987@gmail.com>
2015-03-27 20:49:15 +0900

    init

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,10 +7,14 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-    it "should have the right title Home" do
-    	visit '/static_pages/home'
-      	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-      end
+   it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
   end
 
   describe "Help page" do
