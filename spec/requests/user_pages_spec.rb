37faca20da7710378ab165e02e7aec507a07ca91
Rei Araki <rei3987@gmail.com>
2015-03-31 08:56:55 +0900

    ruby mine included

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "UserPages" do
- subject { page }
+subject { page }
 
   describe "signup page" do
     before { visit signup_path }
@@ -10,46 +10,62 @@ describe "UserPages" do
     it { should have_title(full_title('Sign up')) }
   end
   
-  describe "profile page" do
+describe "profile page" do
   let(:user) { FactoryGirl.create(:user) }
-  before { visit user_path(user) }
+   before { visit user_path(user) }
 
   it { should have_content(user.name) }
   it { should have_title(user.name) }
 end
 
- describe "signup" do
+describe "signup" do
 
-    before { visit signup_path }
+  before { visit signup_path }
 
-    let(:submit) { "Create my account" }
+  let(:submit) { "Create my account" }
 
-    describe "with invalid information" do
-      it "should not create a user" do
+  describe "with invalid information" do
+    it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
-      end
+    end
     end
 
     describe "with valid information" do
-      before do
+        before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
-      end
+        end
 
-      it "should create a user" do
+        it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
-      end
+        end
 
         describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by(email: 'user@example.com') }
+          before { click_button submit }
+          let(:user) { User.find_by(email: 'user@example.com') }
 
-        it { should have_link('Sign out') }
-        it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-      end
+          it { should have_link('Sign out') }
+          it { should have_title(user.name) }
+          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        end
     end 
+describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
   end
+end
 end
