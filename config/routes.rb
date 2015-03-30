78f7b8c741aeba221a47933ac55dec27c882b4fb
Rei Araki <rei3987@gmail.com>
2015-03-30 12:19:53 +0900

    do chap7

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
+  resources :users
 
   # get "static_pages/home"
   # get 'static_pages/about'
