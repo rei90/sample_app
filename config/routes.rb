6a3de65ff4c22a5cd2ac33464da594ad65873a6a
Rei Araki <rei3987@gmail.com>
2015-03-30 14:46:20 +0900

    deleted miss branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SampleApp::Application.routes.draw do
   root  'static_pages#home'
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
-  resources :users
-
+  
   # get "static_pages/home"
   # get 'static_pages/about'
   # get 'static_pages/contact'
