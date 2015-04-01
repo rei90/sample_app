bb37d7f55644cc96728e7dc69858bd0e0d8782f4
Rei Araki <rei3987@gmail.com>
2015-04-01 12:11:42 +0900

    dochap 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-	before_action :signed_in_user, only: [:index, :edit, :update]
+	before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
 	before_action :correct_user,   only: [:edit, :update]
+	before_action :admin_user,     only: :destroy
+
 
 	def show
 		@user = User.find(params[:id])
@@ -8,6 +10,13 @@ class UsersController < ApplicationController
 	def new
 	@user = User.new
 	end  
+
+
+def destroy
+	User.find(params[:id]).destroy
+	flash[:success] = "User destroyed."
+	redirect_to users_url
+end
 
 	def edit
 			# @user = User.find(params[:id])
@@ -52,4 +61,8 @@ private
 		@user = User.find(params[:id])
 		redirect_to(root_path) unless current_user?(@user)
 	end
+
+	def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -67,6 +67,19 @@ subject { page }
 			end
 		end
 
+		describe "as non-admin user" do
+		let(:user) { FactoryGirl.create(:user) }
+		let(:non_admin) { FactoryGirl.create(:user) }
+
+			before { sign_in non_admin, no_capybara: true }
+
+				describe "submitting a DELETE request to the Users#destroy action" do
+			 		before { delete user_path(user) }
+			 			specify { expect(response).to redirect_to(root_path) }
+					end
+		end
+
+
 		describe "as wrong user" do
 		let(:user) { FactoryGirl.create(:user) }
 		let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
