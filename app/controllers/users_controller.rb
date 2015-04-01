848f963291abbfacb949f06ce0e777eb17616140
Rei Araki <rei3987@gmail.com>
2015-04-01 10:32:36 +0900

    half done chap9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,55 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-  end
-  def new
-  @user = User.new
-  end  
+	before_action :signed_in_user, only: [:index, :edit, :update]
+	before_action :correct_user,   only: [:edit, :update]
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
+	def show
+		@user = User.find(params[:id])
+	end
+	def new
+	@user = User.new
+	end  
 
-  private
-  def user_params
-  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+	def edit
+			# @user = User.find(params[:id])
+	end
+
+	def index
+		@users = User.all
+	end
+	
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			sign_in @user
+			flash[:success] = "Welcome to the Sample App!"
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
+
+def update
+	# @user = User.find(params[:id])
+	if @user.update_attributes(user_params)
+		flash[:success] = "Profile updated"
+		redirect_to @user
+	else
+		render 'edit'
+	end
+end
+
+
+
+private
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
+	def signed_in_user
+		store_location
+		redirect_to signin_url, notice: "Please sign in." unless signed_in?
+	end
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user?(@user)
+	end
 end
