1ebb19257d448509d6b6487e8767534392fdf504
Rei Araki <rei3987@gmail.com>
2015-04-03 08:36:21 +0900

    11.2.5

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -35,8 +35,22 @@ subject { page }
 			end
 	end
 	describe "authorization" do
+
 		describe "for non-signed-in users" do
 		let(:user) { FactoryGirl.create(:user) }
+
+
+		describe "in the Relationships controller" do
+			describe "submitting to the create action" do
+				before { post relationships_path }
+					specify { expect(response).to redirect_to(signin_path) }
+			end
+
+			describe "submitting to the destroy action" do
+				before { delete relationship_path(1) }
+					specify { expect(response).to redirect_to(signin_path) }
+			end
+		end
 
 			describe "when attempting to visit a protected page" do
 				before do
@@ -63,6 +77,16 @@ subject { page }
 				describe "submitting to the update action" do
 					before { patch user_path(user) }
 						specify { expect(response).to redirect_to(signin_path) }
+				end
+
+				describe "visiting the following page" do
+					before { visit following_user_path(user) }
+					it { should have_title('Sign in') }
+				end
+
+				describe "visiting the followers page" do
+					before { visit followers_user_path(user) }
+					it { should have_title('Sign in') }
 				end
 			end
 
